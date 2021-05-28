6c48002604a7689a2239d4adf9f0d3261d764cda
mai nguyen <maing528@hotmail.com>
2021-05-28 15:29:59 -0700

    Initial Commit

---
+++ MusicU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicU
+//
+//  Created by mai ng on 5/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
